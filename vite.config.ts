620bca916e4bb5f1a7e3a8e2c5e7b09b096e5886
Willy Indra <willyindra21@gmail.com>
2025-09-18 21:06:46 +0700

    Fix TS + Vite config for Vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path, { dirname } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 // __dirname replacement in ESM
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
